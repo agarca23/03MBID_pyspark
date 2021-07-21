b9f7b93abcb776e045d3f07a418c2b0221d9421b
agarca23 <agarca23@estudiantes.unileon.es>
2021-07-21 20:57:43 +0200

    eliminadas lamda

--- personaGastosConTarjetaCredito.py
+++ personaGastosConTarjetaCredito.py
@@ -3,6 +3,17 @@
 import sys
 from pyspark import SparkContext, SparkConf
 
+
+def  dividirYTarjeta (linea):
+    nombre, metodo, importe = linea.split(";")
+    if(metodo == "Tarjeta de crédito"):
+        return nombre, importe
+
+def obtenerSuma(val1, val2):
+    return val1+val2
+
+    
+
 conf = SparkConf().setMaster("local").setAppName("mi programa")
 sc = SparkContext(conf = conf)
 
@@ -13,15 +24,5 @@ print(salida)
 
 datosEntrada = sc.textFile(entrada)
 
-
-
-
-def  dividirYTarjeta (linea):
-    nombre, metodo, importe = linea.split(";")
-    if(metodo == "Tarjeta de crédito"):
-        return nombre, importe
-
-
-
-gastoTarjeta = datosEntrada.flatMap(dividirYTarjeta).reduceByKey(lambda x, y: x+y)
+gastoTarjeta = datosEntrada.flatMap(dividirYTarjeta).reduceByKey(obtenerSuma)
 gastoTarjeta.saveAsTextFile(salida)
